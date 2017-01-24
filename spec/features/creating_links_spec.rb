9d451ae8d9c2eaf73820d2e348b2b3b251004a52
Paul Goodman <paulyjgooders@gmail.com>
2017-01-24 12:24:27 +0000

    #12. clean up database

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,16 +1,25 @@
 require 'spec_helper'
+require 'database_cleaner'
+
 
 feature 'Creating links' do
-	scenario "Add the site's address and title to bookmark manager" do
-	visit('/links/new')
-	fill_in('title', with: "Makers Academy")
-	fill_in('url', with: "http://www.makersacademy.com")
-	click_button('submit')
-	
-		expect(current_path).to eq '/links'
-		within 'ul#links' do
-			expect(page).to have_content("Makers Academy")
-			expect(page).to have_content("http://www.makersacademy.com")
-		end
+
+	before(:each) do
+		DatabaseCleaner.strategy = :truncation
+		# then, whenever you need to clean the DB
+		DatabaseCleaner.clean
 	end
+
+		scenario "Add the site's address and title to bookmark manager" do
+		visit('/links/new')
+		fill_in('title', with: "Makers Academy")
+		fill_in('url', with: "http://www.makersacademy.com")
+		click_button('submit')
+
+			expect(current_path).to eq '/links'
+			within 'ul#links' do
+				expect(page).to have_content("Makers Academy")
+				expect(page).to have_content("http://www.makersacademy.com")
+			end
+		end
 end
