46a962e18ce1ce720b257ad85f3f5131c714dea2
Paul Goodman <paulyjgooders@gmail.com>
2017-01-24 15:22:01 +0000

    #13. configuring Rack ENV (WIP)

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -4,12 +4,6 @@ require 'database_cleaner'
 
 feature 'Creating links' do
 
-	before(:each) do
-		DatabaseCleaner.strategy = :truncation
-		# then, whenever you need to clean the DB
-		DatabaseCleaner.clean
-	end
-
 		scenario "Add the site's address and title to bookmark manager" do
 		visit('/links/new')
 		fill_in('title', with: "Makers Academy")
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -102,4 +102,21 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+# Everything in this block runs once before all the tests run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
