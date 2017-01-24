9d2ac602f0e79ecc07066ec43b74361eae61a3cd
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-24 12:11:18 +0000

    Update #11

--- app/app.rb
+++ app/app.rb
@@ -10,11 +10,20 @@ class BookmarkManager < Sinatra::Base
   # sets the view directory correctly
   set :views, Proc.new { File.join(root, "views") }
 
-  get '/links' do
+    get '/links' do
     # This uses DataMapper's .all method to fetch all
     # data pertaining to this class from the database
     @links = Link.all
     erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
