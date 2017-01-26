1ee04df79be53ff8b9c4d61b2bf9f7318afeb5ab
James Turner <mail.jt@gmail.com>
2017-01-26 15:47:32 +0000

    step 19

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,11 +1,20 @@
-feature 'registering users' do 
-
+feature 'registering users' do
   scenario 'regitering a user' do
   	visit('/register')
   	fill_in :email, with: 'my_email@email.com'
   	fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
   	expect{ click_button 'Sign up' }.to change(User,:count).by(1)
   	expect(current_path).to eq '/links'
   	expect(page).to have_content('Welcome to bookmark manager, my_email@email.com')
+  end
+  scenario 'bad confirmation password' do
+    visit('/register')
+  	fill_in :email, with: 'my_email@email.com'
+  	fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password1234'
+  	expect{ click_button 'Sign up' }.to change(User,:count).by(0)
+
+
   end
 end
